0dec850063fcb4b1f4cc51db36a7de7039014d3d
John Doe <jack.h.sawyer@pm.me>
2023-05-11 16:21:40 -0700

    created endpoints and hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import menuLinksData from './data/menu_links.json'
+import React, { useState, useEffect } from 'react';
+// import menuLinksData from './data/menu_links.json'
 
 const Header = () => {
+    const [menuLinksData, setMenuLinksData] = useState([]);
+
+    const loadMenuLinksData = async () => {
+        //Query the API gateway
+        const response = await fetch('https://4qd4obh5f3.execute-api.us-east-1.amazonaws.com/Production/menuLinks')
+        let jsonData = await response.json();
+
+        //Assign response data to the state variable
+        setMenuLinksData(jsonData);
+    }
+
+    useEffect(() => {
+        // load the menuLinks data from the API gateway
+        loadMenuLinksData();
+    }, []);
+
+
     return (
         <header id="intro">
             <article className="fullheight">
@@ -18,10 +35,10 @@ const Header = () => {
                     <ul>
                         {
                             menuLinksData.map((link) =>
-                            <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
+                                <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
                             )
                         }
-                        
+
                     </ul>
                 </div>
             </nav>
